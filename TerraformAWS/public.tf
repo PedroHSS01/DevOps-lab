498c7ea9d59f89cad96e48112ef4fed47f2ffeeb
PedroHSS01 <pedrohenrique151999@gmail.com>
2026-06-06 01:20:17 -0300

    feat: cria subnets públicas com mapeamento de ip e tags para elb

--- TerraformAWS/public.tf
+++ TerraformAWS/public.tf
@@ -1,20 +1,28 @@
 resource "aws_subnet" "eks_subnet_public_1a" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
-  availability_zone       = "us-east-1a"
+  availability_zone       = "${data.aws_region.current.name}a"
   map_public_ip_on_launch = true
-  tags = {
-    Name = "labterraformtest-subnet-public-1a"
-  }
+  tags = merge(
+    local.tags,
+    {
+      Name                              = "labterraformtest-subnet-public-1a"
+      "kubernetes.io/role/internal-elb" = 1
+    }
+  )
 }
 
 resource "aws_subnet" "eks_subnet_public_1b" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
-  availability_zone       = "us-east-1b"
+  availability_zone       = "${data.aws_region.current.name}b"
   map_public_ip_on_launch = true
 
-  tags = {
-    Name = "labterraformtest-subnet-public-1b"
-  }
+  tags = merge(
+    local.tags,
+    {
+      Name                              = "labterraformtest-subnet-public-1b"
+      "kubernetes.io/role/internal-elb" = 1
+    }
+  )
 }
