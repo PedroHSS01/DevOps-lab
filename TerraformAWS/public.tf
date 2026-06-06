2612c944924e7d6ed417d8f300948544019ec5b5
PedroHSS01 <pedrohenrique151999@gmail.com>
2026-06-06 01:50:10 -0300

    feat :injetar automaticamente project_name na tag description

--- TerraformAWS/public.tf
+++ TerraformAWS/public.tf
@@ -6,7 +6,7 @@ resource "aws_subnet" "eks_subnet_public_1a" {
   tags = merge(
     local.tags,
     {
-      Name                     = "labterraformtest-subnet-public-1a"
+      Name                     = "${var.project_name}-subnet-public-1a"
       "kubernetes.io/role/elb" = 1
     }
   )
@@ -21,7 +21,7 @@ resource "aws_subnet" "eks_subnet_public_1b" {
   tags = merge(
     local.tags,
     {
-      Name                     = "labterraformtest-subnet-public-1b"
+      Name                     = "${var.project_name}-subnet-public-1b"
       "kubernetes.io/role/elb" = 1
     }
   )
--- TerraformAWS/variables.tf
+++ TerraformAWS/variables.tf
@@ -2,3 +2,8 @@ variable "cidr_block" {
   type        = string
   description = "O bloco CIDR para a VPC"
 }
+
+variable "project_name" {
+  type        = string
+  description = "O nome do projeto para tags dos recursos"
+}
