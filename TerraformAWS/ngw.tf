ff175075d66a175f76ea7bcf9780a9e8a41e18af
PedroHSS01 <pedrohenrique151999@gmail.com>
2026-06-07 19:06:50 -0300

    feat: add private subnet para NAT Gateway

---
+++ TerraformAWS/ngw.tf
@@ -0,0 +1,45 @@
+resource "aws_eip" "eks_ngw_eip_1a" {
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-eip_1a"
+    }
+  )
+}
+
+resource "aws_eip" "eks_ngw_eip_1b" {
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-eip_1b"
+    }
+  )
+}
+
+resource "aws_nat_gateway" "eks_ngw_1a" {
+  allocation_id = aws_eip.eks_ngw_eip_1a.id
+  subnet_id     = aws_subnet.eks_subnet_private_1a.id
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-ngw_1a"
+    }
+  )
+
+}
+
+resource "aws_nat_gateway" "eks_ngw_1b" {
+  allocation_id = aws_eip.eks_ngw_eip_1b.id
+  subnet_id     = aws_subnet.eks_subnet_private_1b.id
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-ngw_1b "
+    }
+  )
+
+}
