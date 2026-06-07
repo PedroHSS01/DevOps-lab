2a4301fc25f6d45cc0fa0bd71a65e1b275a70aca
PedroHSS01 <pedrohenrique151999@gmail.com>
2026-06-07 19:21:15 -0300

    feat: atualizar região das tabelas de rotas públicas

--- TerraformAWS/public.tf
+++ TerraformAWS/public.tf
@@ -34,6 +34,6 @@ resource "aws_route_table_association" "eks_public_rtb_association_1a" {
 }
 
 resource "aws_route_table_association" "eks_public_rtb_association_1b" {
-  subnet_id      = aws_subnet.eks_subnet_public_1a.id
+  subnet_id      = aws_subnet.eks_subnet_public_1b.id
   route_table_id = aws_route_table.eks_public_route_table.id
 }
