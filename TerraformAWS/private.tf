cf39f761e7aaf07c085f15d914f7f0557282ea0a
PedroHSS01 <pedrohenrique151999@gmail.com>
2026-06-07 19:38:42 -0300

    feat: add rotas de tabela privadas

--- TerraformAWS/private.tf
+++ TerraformAWS/private.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "eks_subnet_private_1a" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = cidrsubnet(var.cidr_block, 8, 3)
-  availability_zone = "${data.aws_region.current.name}a"
+  availability_zone = "${data.aws_region.current.region}a"
 
   tags = merge(
     local.tags,
@@ -15,7 +15,7 @@ resource "aws_subnet" "eks_subnet_private_1a" {
 resource "aws_subnet" "eks_subnet_private_1b" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = cidrsubnet(var.cidr_block, 8, 4)
-  availability_zone = "${data.aws_region.current.name}b"
+  availability_zone = "${data.aws_region.current.region}b"
 
   tags = merge(
     local.tags,
@@ -24,4 +24,15 @@ resource "aws_subnet" "eks_subnet_private_1b" {
       "kubernetes.io/role/internal-elb" = 1
     }
   )
+}
+
+
+resource "aws_route_table_association" "eks_public_rtb_association_private_1a" {
+  subnet_id      = aws_subnet.eks_subnet_private_1a.id
+  route_table_id = aws_route_table.eks_private_route_table_1a.id
+}
+
+resource "aws_route_table_association" "eks_public_rtb_association_private_1b" {
+  subnet_id      = aws_subnet.eks_subnet_private_1b.id
+  route_table_id = aws_route_table.eks_private_route_table_1b.id
 }
