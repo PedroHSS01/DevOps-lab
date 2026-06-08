3b0a476a0245700970f0da827bade22d03c4b353
PedroHSS01 <pedrohenrique151999@gmail.com>
2026-06-07 21:04:15 -0300

    feat: criação do módulo eks_network com VPC, subnets, IGW e NGW

---
+++ TerraformAWS/modules/network/variables.tf
@@ -0,0 +1,9 @@
+variable "cidr_block" {
+  type        = string
+  description = "O bloco CIDR para a VPC"
+}
+
+variable "project_name" {
+  type        = string
+  description = "O nome do projeto para tags dos recursos"
+}
