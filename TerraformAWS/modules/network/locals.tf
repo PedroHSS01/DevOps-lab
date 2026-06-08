1c1e2c33aebb9a5209179da9f44d50629633282e
PedroHSS01 <pedrohenrique151999@gmail.com>
2026-06-07 21:22:43 -0300

    fix terraform fmt

--- TerraformAWS/modules/network/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  tags = {
-    Owner        = "Pedro Sousa"
-    Department   = "DevOps"
-    Organization = "Infraestrutura e Cloud"
-    Environment  = "Development"
-    Project      = "Terraform EKS AWS"
-  }
-}
--- TerraformAWS/modules/network/variables.tf
+++ TerraformAWS/modules/network/variables.tf
@@ -7,3 +7,8 @@ variable "project_name" {
   type        = string
   description = "O nome do projeto para tags dos recursos"
 }
+
+variable "tags" {
+  type        = map(any)
+  description = "Tags para ser adicionado aos recursos da AWS"
+}
