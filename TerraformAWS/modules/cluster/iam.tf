95d734c97db9e150692caa0266f95222b0436df7
PedroHSS01 <pedrohenrique151999@gmail.com>
2026-06-07 22:26:47 -0300

    feat: add EKS cluster e IAM role

---
+++ TerraformAWS/modules/cluster/iam.tf
@@ -0,0 +1,28 @@
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "${var.project_name}-eks-cluster-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+  tags = merge(
+    var.tags,
+    {
+      name = "${var.project_name}-eks-cluster-role"
+    }
+  )
+}
+
+resource "aws_iam_policy_attachment" "eks_cluster_role_attachment" {
+  name       = "${var.project_name}-eks-cluster-role-attachment"
+  roles      = [aws_iam_role.eks_cluster_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
