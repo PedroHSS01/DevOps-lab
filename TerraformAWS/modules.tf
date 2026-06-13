0d47e230b4a3b8d9af900c89129f1345f55e3fde
PedroHSS01 <pedrohenrique151999@gmail.com>
2026-06-12 22:42:05 -0300

    fix(eks): correct iam policy attachment reference in cluster.tf

--- TerraformAWS/modules.tf
+++ TerraformAWS/modules.tf
@@ -6,7 +6,9 @@ module "eks_network" {
 }
 
 module "eks_cluster" {
-  source       = "./modules/cluster"
-  project_name = var.project_name
-  tags         = local.tags
+  source           = "./modules/cluster"
+  project_name     = var.project_name
+  tags             = local.tags
+  public_subnet_1a = module.eks_network.public_subnet_1a
+  public_subnet_1b = module.eks_network.public_subnet_1b
 }
--- TerraformAWS/modules/cluster/variables.tf
+++ TerraformAWS/modules/cluster/variables.tf
@@ -7,3 +7,14 @@ variable "tags" {
   type        = map(any)
   description = "Tags para ser adicionado aos recursos da AWS"
 }
+
+
+variable "public_subnet_1a" {
+  type        = string
+  description = "ID da Subnet Pública na Zona de Disponibilidade 1a"
+}
+
+variable "public_subnet_1b" {
+  type        = string
+  description = "ID da Subnet Pública na Zona de Disponibilidade 1b"
+}
