e448f59fa8a843f3dfffd25b1c66e59b3cf3968a
PedroHSS01 <pedrohenrique151999@gmail.com>
2026-06-22 23:54:38 -0300

    feat: adiciona provider kubernetes, outputs do cluster e passa oidc para o módulo alb controller

--- TerraformAWS/provider.tf
+++ TerraformAWS/provider.tf
@@ -4,9 +4,23 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.49.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "3.2.0"
+    }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
+}
+
+provider "kubernetes" {
+  host                   = module.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
+  exec {
+    api_version = "client.authentication.k8s.io/v1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+    command     = "aws"
+  }
 }
